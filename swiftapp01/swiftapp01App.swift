9e7661a57549458e4256c4b605c6f6cd6fdbd058
Steven Hertz <StevenH@appi4.com>
2024-04-08 23:52:17 -0400

    Initial Commit

---
+++ swiftapp01/swiftapp01App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftapp01App.swift
+//  swiftapp01
+//
+//  Created by Steven Hertz on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftapp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
