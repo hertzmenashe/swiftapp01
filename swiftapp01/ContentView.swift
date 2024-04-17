19e8af12ca24405ddde6dc7a06ec00f3a41fdd66
Steven Hertz <StevenH@appi4.com>
2024-04-17 11:00:42 -0400

    vstack to list

--- swiftapp01/ContentView.swift
+++ swiftapp01/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        List {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
