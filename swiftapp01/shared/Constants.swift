a620f00a28b6129b1db6eac8b5048a625fc67d04
Steven Hertz <StevenH@appi4.com>
2024-04-17 10:07:25 -0400

    First start before push

---
+++ swiftapp01/shared/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  swiftapp01
+//
+//  Created by Steven Hertz on 4/9/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
+
---
+++ swiftapp01/shared/DatabaseHelper.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  swiftapp01
+//
+//  Created by Steven Hertz on 4/16/24.
+//
+
+import Foundation
+
+struct DatabaseHelper {
+    func getProducts() async throws -> [String] {
+        guard let url = URL(string: "https://dummyjson.com/products") else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        
+
+        return ["xx"]
+
+    }
+}
+
+// MARK: - Welcome
+struct ProductArray: Codable {
+    let products: [Product]
+    let total, skip, limit: Int
+}
+
+// MARK: - Product
+struct Product: Codable {
+    let id: Int
+    let title, description: String
+    let price: Int
+    let discountPercentage, rating: Double
+    let stock: Int
+    let brand, category: String
+    let thumbnail: String
+    let images: [String]
+}
